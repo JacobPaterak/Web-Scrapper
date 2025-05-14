b0cb0a871a918c6f3d2f216965c7301cef34812f
Jacob Paterak <jacobpaterak@gmail.com>
2025-05-13 21:34:36 -0400

    Added user input

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ counter = 1
 total_plants = []
 total_prices = []
 # plant_names = [plant.find('p', class_='product-card-title').text for plant in plants]
-
+def DisplayAll():
+    for plant in total_plants:
+        print(plant)
 def Search(search):
     for plant,price in zip(total_plants,total_prices):
         if(search == plant):
             print(plant + " was found and it costs " + price)
             return
     print("Plant not found")
-
 
 for page_number in range(1, total_pages + 1):
     url = base_url.format(page_number)
@@ -29,9 +30,20 @@ for page_number in range(1, total_pages + 1):
         total_plants.append(plant.text)
         total_prices.append(price.text)
 
+print("Welcome to Jacobs plants search program")
 
-
-
-search = input("Please input what type of plant you are looking for ")
-Search(search)
+print("\t1,) Do you want to search a plant up by name ")
+print("\t2,) Do you want to list of all the plants available")
+print("\t3,) Do you want to search for all plants of a certain type ")
+print()
+userinput = input("What do you want to do ")
+if(userinput == "1"):
+    userPlant = input("What is the name of the plant you are looking for")
+    Search(userPlant)
+elif(userinput == "2"):
+    DisplayAll()
+elif(userinput == "3"):
+    print("Working on it")
+else:
+    print("Invalid Input")
 
